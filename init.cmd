e23b3cb4f6182301510923fffedee6974a8c6ec6
tenshi <sairi.na.tenshi@gmail.com>
2010-10-17 20:36:51 +0400

    master

--- init.cmd
+++ init.cmd
@@ -1,1 +1,1 @@
-git init && git pull git://github.com/sairi-na-tenshi/wc_std-desktop.git && git submodule init && git submodule update
+git init && git pull git://github.com/sairi-na-tenshi/wc_std-desktop.git master && git submodule init && git submodule update
